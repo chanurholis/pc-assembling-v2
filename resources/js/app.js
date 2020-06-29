d42cf0f956428c11b0d992ef9b2ca724d57448c4
Chacha Nurholis <chachanurholis29@gmail.com>
2020-06-29 18:48:57 +0700

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+// AdminLTE
+require('../adminlte/bower_components/jquery/dist/jquery.min.js');
+require('../adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js');
+require('../adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js');
+require('../adminlte/bower_components/fastclick/lib/fastclick.js');
+require('../adminlte/dist/js/adminlte.min.js');
+require('../adminlte/dist/js/demo.js');
